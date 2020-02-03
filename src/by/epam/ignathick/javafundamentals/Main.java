952e5c87a8c576cacde5a1512f1e2126c15bcfa1
Ignathick Alexandr <shock3r.brest@gmail.com>
2020-02-04 00:03:38 +0300

    make task 1

---
+++ src/by/epam/ignathick/javafundamentals/Main.java
@@ -0,0 +1,10 @@
+package by.epam.ignathick.javafundamentals;
+
+import by.epam.ignathick.javafundamentals.by.epam.ignathick.task1.Task1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Task1 task1 = new Task1(); ///Найти сумму элементов матрицы, расположенных между первым и вторым положительными элементами каждой строки
+    }
+}
---
+++ src/by/epam/ignathick/javafundamentals/by/epam/ignathick/task1/Task1.java
@@ -0,0 +1,71 @@
+package by.epam.ignathick.javafundamentals.by.epam.ignathick.task1;
+
+import java.util.Random;
+
+public class Task1 {
+    int interval = 10;
+    int dimmension = 5;
+    int[][] matrix = new int[dimmension][dimmension];
+
+    public Task1() {
+        putValuesIntoTheMatix();
+        calculateSumm();
+    }
+
+    public Task1(int interval, int dimmension) {
+        this.interval = interval;
+        this.dimmension = dimmension;
+
+        putValuesIntoTheMatix();
+        calculateSumm();
+
+    }
+
+    private void putValuesIntoTheMatix() {
+
+        int min = -interval;
+        int max = interval;
+
+        for (int i = 0; i < dimmension; i++) {
+            for (int j = 0; j < dimmension; j++) {
+                matrix[i][j] = new Random().nextInt((max - min) + 1) + min;
+            }
+        }
+
+        System.out.println(printMatrix());
+    }
+
+    private void calculateSumm(){
+        int sum = 0;
+        for (int i = 0; i < dimmension; i++) {
+            int firstPositiveValueInRow = 0;
+            int lastPositiveValueInRow = 0;
+            for (int j = 0; j < dimmension; j++) {
+                int curentMatrixValue = matrix[i][j];
+                if (curentMatrixValue > 0) {
+                    if(firstPositiveValueInRow == 0) {
+                        firstPositiveValueInRow = curentMatrixValue;
+                    } else if (lastPositiveValueInRow == 0) {
+                        lastPositiveValueInRow = curentMatrixValue;
+                    }
+                } else if (firstPositiveValueInRow > 0 && lastPositiveValueInRow == 0) {
+                    sum += curentMatrixValue;
+                }
+            }
+        }
+        System.out.println("Summ between first and last positive values in all rows of matrix is: " + sum);
+    }
+
+    public String printMatrix() {
+        String matrixOut = "";
+        for (int i = 0; i < dimmension; i++) {
+            for (int j = 0; j < dimmension; j++) {
+                matrixOut += matrix[i][j];
+                matrixOut += "\t";
+            }
+            matrixOut = matrixOut + "\n";
+        }
+        return  matrixOut;
+    }
+
+}
